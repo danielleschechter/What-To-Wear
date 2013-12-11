15af51dfd91fb28d95dfacf5308dde45d8941c31
Danielle Schechter <danielle@danielleschechter.com>
2013-12-10 19:39:56 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhatToWear::Application.config.secret_key_base = '5ccb2b08e2fd100888ab17010f6dadd1fbdc15dba31b74004edfff6d1b8f7a1be141675dd170650bf288a012979a4589c9c26130416225055935eff3981e5e0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhatToWear::Application.config.session_store :cookie_store, key: '_What_To_Wear_session'
