2e912a68c532205385888768e3ff43beb3601209
Danielle Schechter <danielle@danielleschechter.com>
2013-12-10 21:59:31 -0500

    changed README file

---
+++ config/initializers/tumblr.rb
@@ -0,0 +1,6 @@
+Tumblr.configure do |config|
+  config.consumer_key = "NBcrIDJcCPZRI8nwSRStemeSQoopM8p3m3mbFgZCkfDLBjc5Ri"
+  config.consumer_secret = "Lp8KJTiJiMpb8gxoKOBK27MQrQIksqFe9SqZfNDREYCmcQwwrF"
+  config.oauth_token = "NBcrIDJcCPZRI8nwSRStemeSQoopM8p3m3mbFgZCkfDLBjc5Ri"
+  config.oauth_token_secret = "Lp8KJTiJiMpb8gxoKOBK27MQrQIksqFe9SqZfNDREYCmcQwwrF"
+end
